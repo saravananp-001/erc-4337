c56d34c66acad85453f82f5687219ded6ee6582e
saravananp-001 <saravananpmech001@gmail.com>
2024-07-29 16:41:53 +0530

    bundler understand

--- scripts/balance.js
+++ scripts/balance.js
@@ -4,6 +4,7 @@ const sender_mysmartAccount = "0x29403ae0a7d2d04d06ffdf87a4943537ce309195";
 const ENTRYPOINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 const FACTORY_ADDRESS = "0x5C119453c85f2318033390Bd4D05DBA7106B4E9f";
 const PAYMASTER_ADDRESS = "0x41931204Cdcd6Ed02A66c5285ab62889B3d3688b";
+const PAYMASTER_ADDRESS1 = "0x34E0fEf5e0116669Ee11A7a0ab520c70eB010B4C";
 async function main() {
 
   const EPoint = await hre.ethers.getContractAt("EntryPoint", ENTRYPOINT_ADDRESS);
@@ -14,6 +15,7 @@ async function main() {
   console.log("Deposit ENTRYPOINT_ADDRESS Balance:", await EPoint.balanceOf(ENTRYPOINT_ADDRESS));
   console.log("Deposit FACTORY_ADDRESS Balance:", await EPoint.balanceOf(FACTORY_ADDRESS));
   console.log("Deposit PAYMASTER_ADDRESS Balance:", await EPoint.balanceOf(PAYMASTER_ADDRESS));
+  console.log("Deposit PAYMASTER_ADDRESS Balance:", await EPoint.balanceOf(PAYMASTER_ADDRESS1));
 }
 
 // Run the main function and catch any errors
